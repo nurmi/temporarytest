4c1130784eee719eef6170ad20135d7a4f49b15a
Daniel Nurmi <nurmi@anchore.com>
2023-08-22 16:29:09 -0700

    update
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- testthing.sh
+++ testthing.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo HELLO WORLD
+curl -SsL https://api.github.com/repos/nurmi/temporarytest/tags/v1.0.0
+curl -SsL https://api.github.com/repos/nurmi/temporarytest/tags/v1.0.0 | jq .prerelease
