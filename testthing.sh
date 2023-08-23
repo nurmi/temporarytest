8d2c6f4bd6729ac75d398df889de8eb741168090
Daniel Nurmi <nurmi@anchore.com>
2023-08-22 17:00:40 -0700

    update
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- testthing.sh
+++ testthing.sh
@@ -4,5 +4,5 @@
 env
 
 echo HELLO WORLD
-curl -H "Authorization: Bearer ${GITHUB_TOKEN}" -SsL https://api.github.com/repos/anchore/anchorectl/tags/v1.8.0
-curl -H "Authorization: Bearer ${GITHUB_TOKEN}" -SsL https://api.github.com/repos/nurmi/temporarytest/tags/v1.0.0 | jq .prerelease
+curl -H "Authorization: Bearer ${GITHUB_TOKEN}" -SsL https://api.github.com/repos/nurmi/temporarytest/releases/tags/v1.0.0
+curl -H "Authorization: Bearer ${GITHUB_TOKEN}" -SsL https://api.github.com/repos/nurmi/temporarytest/releases/tags/v1.0.0 | jq .prerelease
